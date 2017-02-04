37c2ba021831e76ca0ba6dd24a8c164717610db4
Tom Richards <tom@tomrichards.net>
2017-02-04 16:36:34 -0500

    Fix worker startup

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -11,8 +11,8 @@ defmodule TicTacToe do
     children = [
       # Start the endpoint when the application starts
       supervisor(TicTacToe.Endpoint, []),
-      worker(TicTacToe.Worker, []),
-      worker(ConCache, [[], [name: :game_state]])
+      worker(ConCache, [[], [name: :game_state]]),
+      worker(TicTacToe.Worker, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
