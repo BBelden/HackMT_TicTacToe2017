a8841d820774daf8be2e71710b9975b3446997b5
Tom Richards <tom@tomrichards.net>
2017-02-04 01:05:11 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TicTacToe.Mixfile do
 
   def project do
     [app: :tic_tac_toe,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
