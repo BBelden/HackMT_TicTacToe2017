d53944a73f8216ee38b155018ef51cff0a76b1b6
Ethan Willis <ethancharleswillis@gmail.com>
2017-02-04 14:05:14 -0600

    Resolving Conflicts

--- web/channels/game_channel.ex
+++ web/channels/game_channel.ex
@@ -9,14 +9,11 @@ defmodule TicTacToe.GameChannel do
   def join("game:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
-<<<<<<< HEAD
-
-=======
 
   def handle_in("vote", %{"vote" => vote}, socket) do
     IO.puts(vote)
     {:noreply, socket}
->>>>>>> 64a5ffab4e771c5b0f4307d7e116baa185c9cc72
+
   def tick do
     IO.inspect(:tick)
   end
