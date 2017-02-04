5aa1e25ffb0f0d9d1529d130026755bc7ad78635
Tom Richards <tom@tomrichards.net>
2017-02-04 10:26:43 -0500

    Unconditionally join team x

--- web/channels/game_channel.ex
+++ web/channels/game_channel.ex
@@ -2,9 +2,10 @@ defmodule TicTacToe.GameChannel do
   use Phoenix.Channel
 
   def join("game:lobby", _message, socket) do
-    IO.puts "game lobby joined by foo"
-    {:ok, socket}
+    team = :x
+    {:ok, team, assign(socket, :team, team)}
   end
+
   def join("game:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
