775e13b1c7942117d50b8d63e86f931cdaace2b4
Ethan Willis <ethancharleswillis@gmail.com>
2017-02-04 21:38:30 -0600

    Passed array to the user upon joining

--- web/channels/game_channel.ex
+++ web/channels/game_channel.ex
@@ -2,6 +2,7 @@ defmodule TicTacToe.GameChannel do
   use Phoenix.Channel
 
   def join("game:lobby", _message, socket) do
+
     team = TicTacToe.Worker.get_value(:teamPlayer)
     cond do
       team == :o -> :x
@@ -11,7 +12,7 @@ defmodule TicTacToe.GameChannel do
         TicTacToe.Worker.put_value(:teamPlayer, :o)
       end
     IO.puts(TicTacToe.Worker.get_value(:teamPlayer))
-    {:ok, team, assign(socket, :teamPlayer, team)}
+    {:ok, [Map.values(TicTacToe.Worker.get_cache())], socket}
   end
 
   def join("game:" <> _private_room_id, _params, _socket) do
@@ -21,7 +22,7 @@ defmodule TicTacToe.GameChannel do
   def handle_in("vote", %{"vote" => vote}, socket) do
     my_team = IO.inspect(Map.get(socket.assigns, :teamPlayer))
     TicTacToe.Worker.apply_vote(my_team, vote)
-    {:reply, {:ok, %{"vote" => vote, "my_team" => my_team}}, socket}
+    {:reply, {:ok, TicTacToe.Worker.get_cache()}, socket}
   end
 
   def tick(time_remaining) do
